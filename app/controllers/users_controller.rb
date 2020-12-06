24dfe6ca9a6c0d1f3ef9184ae7bfa7e272f7676a
Tchalaou33 <attchalaou@gmail.com>
2020-12-06 06:32:53 -0500

    added logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
 
 # Signup
     def create
+        @user = User.new(user_params)
+        if @user.save
+            #login
+            session[:user_id] = @user.id
+            redirect_to routines_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
