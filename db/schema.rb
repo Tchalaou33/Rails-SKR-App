f158d2196f2ea4e83875e8942b4116ec94096afe
Tchalaou33 <attchalaou@gmail.com>
2020-12-07 02:29:38 -0500

    mirgrated Routine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_190705) do
+ActiveRecord::Schema.define(version: 2020_12_06_194521) do
+
+  create_table "routines", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
