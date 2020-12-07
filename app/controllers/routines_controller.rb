29d4b14f955a0e4ca8f00ca01af1d852a170a191
Tchalaou33 <attchalaou@gmail.com>
2020-12-07 04:53:04 -0500

    created path to submit routine

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -2,13 +2,24 @@ class RoutinesController < ApplicationController
 
     def new
         @routine = Routine.new
-        
     end
 
 
     def create
+        @routine = Routine.new(routine_params)
+
+        if @routine.save
+            redirect_to routine_path(@routine)
+        else
+            render :new
+        end
     end
 
+    # For security 
+    private
+
+    def routine_params
+        params.require(:routine).permit(:title, :season, :step, :product)
+    end
 end
 
-# <%= f.collections_select()
