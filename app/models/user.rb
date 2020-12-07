2e2ae1bd16b15d4f74163c268a23f5a48f582389
Tchalaou33 <attchalaou@gmail.com>
2020-12-07 03:03:27 -0500

    syntax error, corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true 
-    validates :email, uniqueness : true
+    validates :email, uniqueness: true
 
 end
