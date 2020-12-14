f4a975e5afadd11c3015e6f9be9325291e0f6d67
Tchalaou33 <attchalaou@gmail.com>
2020-12-14 11:14:16 -0500

    added to products migration belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_152323) do
+ActiveRecord::Schema.define(version: 2020_12_14_160916) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "content"
+    t.integer "user_id"
+    t.integer "review_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "routines", force: :cascade do |t|
