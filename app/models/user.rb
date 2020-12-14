0e6804d10c77d79678e8ad80ecb00fa6beeb3301
Tchalaou33 <attchalaou@gmail.com>
2020-12-14 12:09:50 -0500

    changed styling of login and signup links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :routines
-    has_many :products, through: :routines
+    has_many :routined_products, through: :routines, source: :product
 
-    
+    has_many :products
 
     validates :email, :username, presence: true 
     validates :email, :username, uniqueness: true
