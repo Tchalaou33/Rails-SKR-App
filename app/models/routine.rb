43519c06ca8c25f9394d3ba268cdb1d43e8b9823
Tchalaou33 <attchalaou@gmail.com>
2021-03-26 11:49:23 -0400

    added user_id migration to products, changed products view

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,22 +2,26 @@ class Routine < ApplicationRecord
 
     belongs_to :user
     belongs_to :product
-    accepts_nested_attributes_for :product
+    # accepts_nested_attributes_for :product
 
     validates :title, presence: true
     validates :title, uniqueness: true
-    validate :not_a_duplicate
+    # validate :not_a_duplicate
 
-
-
-    def not_a_duplicate
-        # if there is already an ice cream with that flavor && brand, throw an error
-        routine = Routine.find_by(title: title, product_id: product_id)
-        if !!routine && routine != self
-          errors.add(:title, 'has already been added for that product')
-        end
+    def product_attributes=(product)
+      self.product = Product.find_or_create_by(name: product[:name])
+      self.product.update(product)
     end
 
+# WHY IS THIS HERE?
+    # def not_a_duplicate
+    #     # if there is already an ice cream with that flavor && brand, throw an error
+    #     routine = Routine.find_by(title: title, product_id: product_id)
+    #     if !!routine && routine != self
+          # errors.add(:title, 'has already been added for that product')
+        # end
+    # end
+       
 
     # scope :order_by_season, -> {order(:season)}
     # scope :cleas, -> {where()}
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_111918) do
+ActiveRecord::Schema.define(version: 2021_03_25_162159) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "content"
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
